73f48477011b4798832027b5d24e209a4f6fa30f
harshil2628 <161985277+harshil2628@users.noreply.github.com>
2024-03-03 13:54:11 -0500

    Update FoodCard.js

--- CenProject/frontend/src/components/FoodCard.js
+++ CenProject/frontend/src/components/FoodCard.js
@@ -1,17 +1,20 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-const FoodCard = ({img, title}) => {
+const FoodCard = ({img, title, identifier}) => {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img} />
+      <Card.Img variant="top" src={img}/>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+          <Card.Title>
+            <Link to={{identifier}}>{title}</Link>
+          </Card.Title>
         <Button variant="primary">Save</Button>
       </Card.Body>
     </Card>
   );
-}
+};
 
-
-export default FoodCard
+export default FoodCard;
