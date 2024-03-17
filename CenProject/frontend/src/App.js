e5a896e6ed9f0b16ee7966dba336aeeee36cd4cd
harshil2628 <161985277+harshil2628@users.noreply.github.com>
2024-03-16 23:46:30 -0400

    Update App.js

--- CenProject/frontend/src/App.js
+++ CenProject/frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; //stores location in browser's address bar using URLs
 import FoodCard from './components/FoodCard';
 import FoodInfo from './components/FoodInfo';
-import Search from './components/search'
-import SideBar from './components/sideBar'
-import './style/App.css'
+import Search from './components/search';
+import SideBar from './components/sideBar';
+import './style/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 //Referenced tutorial: https://www.geeksforgeeks.org/reactjs-router/ 
@@ -12,9 +12,9 @@ const App = () =>{
     return (
       <Router>
         <div className="App">
+          <SideBar /> /* Rendered outside the router so it appears on every page instead of just the homepage */
         <Routes>
           <Route path="/" element={<Search/>}></Route>
-          <Route path="/" element={<SideBar/>}></Route>
           <Route path="/" element={<FoodCard/>}></Route>
           <Route path="/" element={<FoodInfo/>}></Route>
         </Routes>
