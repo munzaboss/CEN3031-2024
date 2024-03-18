4ad74eec2efc225f45d2db71e2b62cd283c9c865
munzaboss <muneebsahmad03@gmail.com>
2024-03-18 11:55:15 -0400

    Starting firebase out. Mde login page

--- CenProject/frontend/src/App.js
+++ CenProject/frontend/src/App.js
@@ -1,17 +1,34 @@
 
 import Search from './components/search'
 import SideBar from './components/sideBar'
+import Login from './components/Login'
 import './style/App.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Fragment } from 'react'
 
+
+function HomePage() {
+  return (
+    <>
+      <SideBar/>
+      <Search/>
+    </>
+  );
+}
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
-      <SideBar></SideBar>
-      <Search></Search>
+      <Routes>
+        <Route path = '/login' Component={Login}/>
+        <Route path = '/' Component={HomePage}/>
+      </Routes>
+      
     </div>
+    </Router>
   );
 }
 
