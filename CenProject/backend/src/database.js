270c7fc73f208a35dd4e0541407b0def90d74f51
munzaboss <muneebsahmad03@gmail.com>
2024-04-11 11:40:25 -0400

    made login page functional, hid firebase api keys

--- CenProject/backend/src/database.js
+++ CenProject/backend/src/database.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from 'dotenv';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAhZMHGyFyq8Cd3LpqhKqUTjVm77Q-58-s",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "cen3031-cooking-website.firebaseapp.com",
   projectId: "cen3031-cooking-website",
   storageBucket: "cen3031-cooking-website.appspot.com",
@@ -17,16 +16,19 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 
-//Realtime database stuff
-export function writeUserData(userID, name, email){
-  const db = getDatabase();
-  const reference = ref(db, 'users/' + userID)
-  
+
+function createUser(userID, name, email, image){
+  const db = getDatabase(firebaseApp);
+  const reference = 'users/' + userID;
   set(reference, {
-    username: name,
+    uid: userID,
+    name: name,
     email: email,
+    userImage: image
   });
 }
+
+export { createUser }
--- CenProject/frontend/src/components/Login.js
+++ CenProject/frontend/src/components/Login.js
@@ -1,27 +1,43 @@
 import { useEffect } from 'react';
 import axios from 'axios';
-import { getAuth } from 'firebase/auth';
-import firebase from 'firebase/compat/app'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { useNavigate } from 'react-router-dom';
 
-function TestupdateUser() {
-    axios.post('http://localhost:8000/createUser', {
-        userID: "4kk4",
-        name: "Muneeeeeb",
-        email: "bruh@mail"
-    })
-    .then(response =>{
-        console.log('lego');
-    })
-    .catch(error =>{
-        console.log('aww man')
-    })
-}
+
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "cen3031-cooking-website.firebaseapp.com",
+    projectId: "cen3031-cooking-website",
+    storageBucket: "cen3031-cooking-website.appspot.com",
+    messagingSenderId: "866186810186",
+    appId: "1:866186810186:web:2b722bf1350ad56301e7b3",
+    measurementId: "G-R4RZ6KL2T0"
+  };
+  
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
 function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try{
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            navigate("/");
+            console.log("Login succesful");
+        } catch (error) {
+            console.log("Login Failed", error.code, error.message)
+        }
+    }
     return (
         <div>
             <h1> Login Page </h1>
-            <button className="submitButton" onClick= {TestupdateUser}> Login </button>
+            <button className="submitButton" onClick= {handleLogin}> Login </button>
         </div>
     );
 }
